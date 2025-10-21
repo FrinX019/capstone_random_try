e64d85ee0730e41a616885773f68bf9415f4a73e
frinx019 <frinpatel1841974@gmail.com>
2025-10-21 18:40:30 -0400

    Initial commit

---
+++ src/main/java/org/example/CapstoneApp.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CapstoneApp extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneApp.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Capstone Project Running!";
+    }
+}
