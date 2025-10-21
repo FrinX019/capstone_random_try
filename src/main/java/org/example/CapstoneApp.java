6bee72169688a91bb7ce44b60c5d46ff4f49e826
frinx019 <frinpatel1841974@gmail.com>
2025-10-21 19:04:16 -0400

    sentance changes

--- src/main/java/org/example/CapstoneApp.java
+++ src/main/java/org/example/CapstoneApp.java
@@ -17,6 +17,6 @@ public class CapstoneApp extends SpringBootServletInitializer {
 class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Capstone Project Running! hello from bhakti is branch";
+        return "Capstone Project Running! hello from frin's branch";
     }
 }
